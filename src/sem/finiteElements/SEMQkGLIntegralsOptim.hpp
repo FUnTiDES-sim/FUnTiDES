55f4c3f60e1ba2eca5a7b6f27e1bdcbf308fd828
Henri Calandra <henricalandra@netscape.net>
2024-12-12 10:34:38 +0100

    replace double with float

--- src/sem/finiteElements/SEMQkGLIntegralsOptim.hpp
+++ src/sem/finiteElements/SEMQkGLIntegralsOptim.hpp
@@ -49,7 +49,7 @@ public:
    * @note @p srcSymMatrix can contain integers but @p dstMatrix must contain floating point values.
   */
   PROXY_HOST_DEVICE 
-  void symInvert( double  dstSymMatrix[6], double  srcSymMatrix[6]) const
+  void symInvert( float  dstSymMatrix[6], float  srcSymMatrix[6]) const
   {
    
      using FloatingPoint = std::decay_t< decltype( dstSymMatrix[ 0 ] ) >;
@@ -58,7 +58,9 @@ public:
      dstSymMatrix[ 5 ] = srcSymMatrix[ 4 ] * srcSymMatrix[ 3 ] - srcSymMatrix[ 5 ] * srcSymMatrix[ 2 ];
      dstSymMatrix[ 4 ] = srcSymMatrix[ 5 ] * srcSymMatrix[ 3 ] - srcSymMatrix[ 4 ] * srcSymMatrix[ 1 ];
    
-     double det = srcSymMatrix[ 0 ] * dstSymMatrix[ 0 ] + srcSymMatrix[ 5 ] * dstSymMatrix[ 5 ] + srcSymMatrix[ 4 ] * dstSymMatrix[ 4 ];
+     float det = srcSymMatrix[ 0 ] * dstSymMatrix[ 0 ] 
+	        + srcSymMatrix[ 5 ] * dstSymMatrix[ 5 ] 
+		+ srcSymMatrix[ 4 ] * dstSymMatrix[ 4 ];
   
      FloatingPoint const invDet = FloatingPoint( 1 ) / det;
    
@@ -78,7 +80,7 @@ public:
    * @note @p symMatrix can contain integers but @p dstMatrix must contain floating point values.
   */
   PROXY_HOST_DEVICE  
-  void symInvert0( double  symMatrix[6] ) const
+  void symInvert0( float  symMatrix[6] ) const
   {
       std::remove_reference_t< decltype( symMatrix[ 0 ] ) > temp[ 6 ];
       symInvert( temp, symMatrix );
@@ -125,15 +127,16 @@ public:
         i1 = (linearIndex%((r+1)*(r+1)))/(r+1);
         i0 = (linearIndex%((r+1)*(r+1)))%(r+1);
   }
-
+  
   template<int ORDER>
-  PROXY_HOST_DEVICE 
-  constexpr static void multiIndex( int const linearIndex, int & i0, int & i1, int & i2 ) 
+  PROXY_HOST_DEVICE
+  constexpr static void multiIndex( int const linearIndex, int & i0, int & i1, int & i2 )
   {
-        i2 = linearIndex/((ORDER+1)*(ORDER+1));
-        i1 = (linearIndex%((ORDER+1)*(ORDER+1)))/(ORDER+1);
         i0 = (linearIndex%((ORDER+1)*(ORDER+1)))%(ORDER+1);
+        i1 = (linearIndex%((ORDER+1)*(ORDER+1)))/(ORDER+1);
+        i2 = linearIndex/((ORDER+1)*(ORDER+1));
   }
+
 
   /**
    * @brief Compute the interpolation coefficients of the q-th quadrature point in a given direction
@@ -142,9 +145,9 @@ public:
    * @return The interpolation coefficient
   */
   PROXY_HOST_DEVICE 
-  constexpr static double interpolationCoord( const int q, const int k ) 
+  constexpr static float  interpolationCoord( const int q, const int k ) 
   {
-      const double alpha = (SEMQkGLBasisFunctions::parentSupportCoord<SEMinfo>( q ) + 1.0 ) / 2.0;
+      const float alpha = (SEMQkGLBasisFunctions::parentSupportCoord<SEMinfo>( q ) + 1.0 ) / 2.0;
       return k == 0 ? ( 1.0 - alpha ) : alpha;
    }
 
@@ -160,7 +163,7 @@ public:
    * @return The value of the jacobian factor
   */
   PROXY_HOST_DEVICE 
-  constexpr static double jacobianCoefficient1D(  const int q, const int i, const int k, const int dir )
+  constexpr static float  jacobianCoefficient1D(  const int q, const int i, const int k, const int dir )
   {
       if( i == dir )
       {
@@ -173,6 +176,14 @@ public:
   }
 
   
+  PROXY_HOST_DEVICE
+  float determinant(float  m[3][3]) const
+  {
+     return abs(m[0][0]*(m[1][1]*m[2][2]-m[2][1]*m[1][2])
+               -m[0][1]*(m[1][0]*m[2][2]-m[2][0]*m[1][2])
+               +m[0][2]*(m[1][0]*m[2][1]-m[2][0]*m[1][1]));
+  }
+
   PROXY_HOST_DEVICE
   double determinant(double  m[3][3]) const
   {
@@ -194,31 +205,37 @@ public:
    void jacobianTransformation( int const qa,
                                 int const qb,
                                 int const qc,
-                                double const (&X)[8][3],
-                                double ( & J )[3][3] ) const
+                                float const (&X)[8][3],
+                                float ( & J )[3][3] ) const
    {
-      for( int k = 0; k < 8; k++ )
+      constexpr int SEQK=8;
+      constexpr int SEQJ=3;
+      constexpr int SEQI=3;
+      //for( int k = 0; k < 8; k++ )
+      loop([&](auto const k)
       {
-        const int ka = k % 2;
-        const int kb = ( k % 4 ) / 2;
-        const int kc = k / 4;
-        for( int j = 0; j < 3; j++ )
+        constexpr int ka = k % 2;
+        constexpr int kb = ( k % 4 ) / 2;
+        constexpr int kc = k / 4;
+        //for( int j = 0; j < 3; j++ )
+        loop([&](auto const j)
         {
-          double jacCoeff = jacobianCoefficient1D( qa, 0, ka, j ) *
+          float  jacCoeff = jacobianCoefficient1D( qa, 0, ka, j ) *
                             jacobianCoefficient1D( qb, 1, kb, j ) *
                             jacobianCoefficient1D( qc, 2, kc, j );
-          for( int i = 0; i < 3; i++ )
+          //for( int i = 0; i < 3; i++ )
+          loop([&](auto const i)
           {
             J[i][j] +=  jacCoeff * X[k][i];
-          }
-        }
-      }
+          },std::make_integer_sequence<int,SEQI>{});
+        },std::make_integer_sequence<int,SEQJ>{});
+      },std::make_integer_sequence<int,SEQK>{});
    }
 
    template<int qa,int qb,int qc>
    PROXY_HOST_DEVICE
-   void jacobianTransformation( double const (&X)[8][3],
-                                double ( & J )[3][3] ) const
+   void jacobianTransformation( float const (&X)[8][3],
+                                float ( & J )[3][3] ) const
    {
       constexpr int SEQK=8;
       constexpr int SEQJ=3;
@@ -231,7 +248,7 @@ public:
         //for( int j = 0; j < 3; j++ )
         loop([&](auto const j)
         {
-          constexpr double jacCoeff = jacobianCoefficient1D(qa, 0, ka, j ) *
+          constexpr float jacCoeff = jacobianCoefficient1D(qa, 0, ka, j ) *
                                       jacobianCoefficient1D(qb, 1, kb, j ) *
                                       jacobianCoefficient1D(qc, 2, kc, j );
           //for( int i = 0; i < 3; i++ )
@@ -255,23 +272,12 @@ public:
   */
   PROXY_HOST_DEVICE
   void computeBMatrix( int const qa, int const qb, int const qc,
-                       double const (&X)[8][3],
-                       double (& J)[3][3],
-                       double (& B)[6] ) const
+                       float const (&X)[8][3],
+                       float (& J)[3][3],
+                       float (& B)[6] ) const
   {
-
-      J[0][0]=0;
-      J[0][1]=0;
-      J[0][2]=0;
-      J[1][0]=0;
-      J[1][1]=0;
-      J[1][2]=0;
-      J[2][0]=0;
-      J[2][1]=0;
-      J[2][2]=0;
-
       jacobianTransformation( qa, qb, qc, X, J );
-      double const detJ = determinant( J );
+      float const detJ = determinant( J );
 
       // compute J^T.J/det(J), using Voigt notation for B
       B[0] = (J[0][0]*J[0][0]+J[1][0]*J[1][0]+J[2][0]*J[2][0])/detJ;
@@ -287,13 +293,13 @@ public:
 
   template<int qa,int qb,int qc>
   PROXY_HOST_DEVICE 
-  void computeBMatrix( double const (&X)[8][3],
-                       double (& J)[3][3],
-                       double (& B)[6] ) const
+  void computeBMatrix( float const (&X)[8][3],
+                       float (& J)[3][3],
+                       float (& B)[6] ) const
   {
       //jacobianTransformation<qa, qb, qc>( X, J );
       jacobianTransformation(qa, qb, qc, X, J );
-      double const detJ = determinant( J );
+      float const detJ = determinant( J );
 
   
       // compute J^T.J/det(J), using Voigt notation for B
@@ -321,30 +327,30 @@ public:
   void computeGradPhiBGradPhi( int const qa,
                                int const qb,
                                int const qc,
-                               double const (&B)[6],
+                               float const (&B)[6],
                                FUNC && func ) const
   {
-     const double w = SEMQkGLBasisFunctions::weight<SEMinfo>(qa )*
+     const float w = SEMQkGLBasisFunctions::weight<SEMinfo>(qa )*
                       SEMQkGLBasisFunctions::weight<SEMinfo>(qb )*
                       SEMQkGLBasisFunctions::weight<SEMinfo>(qc );
-     //loop( [&] (auto const i)
-     for(int i=0;i<ORDER+1;i++)
+     //for(int i=0;i<ORDER+1;i++)
+     loop( [&] (auto const i)
      {
        const int ibc = linearIndex< ORDER>(i, qb, qc );
        const int aic = linearIndex< ORDER>(qa, i, qc );
        const int abi = linearIndex< ORDER>(qa, qb, i );
-       const double gia = SEMQkGLBasisFunctions::basisGradientAt( i, qa );
-       const double gib = SEMQkGLBasisFunctions::basisGradientAt( i, qb );
-       const double gic = SEMQkGLBasisFunctions::basisGradientAt( i, qc );
-       //loop( [&] (auto const j)
-       for( int j=0;j<ORDER+1;j++)
+       const float gia = SEMQkGLBasisFunctions::basisGradientAt( i, qa );
+       const float gib = SEMQkGLBasisFunctions::basisGradientAt( i, qb );
+       const float gic = SEMQkGLBasisFunctions::basisGradientAt( i, qc );
+       //for( int j=0;j<ORDER+1;j++)
+       loop( [&] (auto const j)
        {
          const int jbc = linearIndex< ORDER>(j, qb, qc );
          const int ajc = linearIndex< ORDER>(qa, j, qc );
          const int abj = linearIndex< ORDER>(qa, qb, j );
-         const double gja = SEMQkGLBasisFunctions::basisGradientAt( j, qa );
-         const double gjb = SEMQkGLBasisFunctions::basisGradientAt( j, qb );
-         const double gjc = SEMQkGLBasisFunctions::basisGradientAt( j, qc );
+         const float gja = SEMQkGLBasisFunctions::basisGradientAt( j, qa );
+         const float gjb = SEMQkGLBasisFunctions::basisGradientAt( j, qb );
+         const float gjc = SEMQkGLBasisFunctions::basisGradientAt( j, qc );
          // diagonal terms
          const double w0 = w * gia * gja;
          func( ibc, jbc, w0 * B[0] );
@@ -353,6 +359,7 @@ public:
          const double w2 = w * gic * gjc;
          func( abi, abj, w2 * B[2] );
          // off-diagonal terms
+	 /*
          const double w3 = w * gib * gjc;
          func( aic, abj, w3 * B[3] );
          func( abj, aic, w3 * B[3] );
@@ -362,13 +369,14 @@ public:
          const double w5 = w * gia * gjb;
          func( ibc, ajc, w5 * B[5] );
          func( ajc, ibc, w5 * B[5] );
-       };//,std::make_integer_sequence<int,ORDER+1>{});
-     };//,std::make_integer_sequence<int,ORDER+1>{});
+	 */
+       },std::make_integer_sequence<int,ORDER+1>{});
+     },std::make_integer_sequence<int,ORDER+1>{});
   }
 
   template<int ORDER,int qa, int qb, int qc,typename FUNC>
   PROXY_HOST_DEVICE
-  void computeGradPhiBGradPhi( double const (&B)[6],
+  void computeGradPhiBGradPhi( float const (&B)[6],
                                FUNC && func ) const
   {
      constexpr double w = SEMQkGLBasisFunctions::weight<SEMinfo>(qa )*
@@ -418,22 +426,23 @@ public:
    * @param X Array containing the coordinates of the mesh support points.
    * @return The diagonal mass term associated to q
   */
+  template<int ORDER>
   PROXY_HOST_DEVICE
-  double computeMassTerm(  int const r, int const q, double const (&X)[8][3] ) const
+  double computeMassTerm(int const q, float const (&X)[8][3] ) const
   {
      int qa, qb, qc;
-     multiIndex( r,q, qa, qb, qc );
+     multiIndex( ORDER,q, qa, qb, qc );
      const double w3D=SEMQkGLBasisFunctions::weight<SEMinfo>( qa )
                      *SEMQkGLBasisFunctions::weight<SEMinfo>( qb )
                      *SEMQkGLBasisFunctions::weight<SEMinfo>( qc );
-     double J[3][3] = {{0}};
+     float J[3][3] = {{0}};
      jacobianTransformation(qa, qb, qc, X, J );
      return determinant( J )*w3D;
   }
 
   template<int ORDER,int q>
   PROXY_HOST_DEVICE
-  double computeMassTerm( double const (&X)[8][3] ) const
+  double computeMassTerm( float const (&X)[8][3] ) const
   {
      //int qa, qb, qc;
     //multiIndex<ORDER>(q, qa, qb, qc );
@@ -443,32 +452,31 @@ public:
      constexpr double w3D=SEMQkGLBasisFunctions::weight<SEMinfo>( qa )
                      *SEMQkGLBasisFunctions::weight<SEMinfo>( qb )
                      *SEMQkGLBasisFunctions::weight<SEMinfo>( qc );
-     double J[3][3] = {{0}};
+     float J[3][3] = {{0}};
      //jacobianTransformation<qa, qb, qc>( X, J );
      jacobianTransformation(qa, qb, qc, X, J );
      return determinant( J )*w3D;
   }
  
   //void computeStiffnessTerm( int const q,
-  template<typename FUNC>
+  template<int ORDER,typename FUNC>
   PROXY_HOST_DEVICE
-  void computeStiffnessTerm( int r,
-                             int const q,
-                             double const (&X)[8][3],
+  void computeStiffnessTerm( int const q,
+                             float const (&X)[8][3],
                              FUNC && func ) const
   {
         int qa, qb, qc;
-        multiIndex( r,q, qa, qb, qc );
-        double B[6] = {0};
-        double J[3][3] = {{0}};
+        multiIndex<ORDER>( q, qa, qb, qc );
+        float B[6] = {0};
+        float J[3][3] = {{0}};
         computeBMatrix( qa, qb, qc, X, J, B );
-        computeGradPhiBGradPhi<SEMinfo::myOrderNumber>( qa, qb, qc, B, func );
+        computeGradPhiBGradPhi<ORDER>( qa, qb, qc, B, func );
   }
 
   template<int ORDER,int q,typename FUNC>
   PROXY_HOST_DEVICE
   void computeStiffnessTerm( 
-                             double const (&X)[8][3],
+                             float const (&X)[8][3],
                              FUNC && func ) const
   {
     //int qa, qb, qc;
@@ -476,8 +484,8 @@ public:
     constexpr int qc = q/((ORDER+1)*(ORDER+1));
     constexpr int qb = (q%((ORDER+1)*(ORDER+1)))/(ORDER+1);
     constexpr int qa = (q%((ORDER+1)*(ORDER+1)))%(ORDER+1);
-    double B[6] = {0};
-    double J[3][3] = {{0}};
+    float B[6] = {0};
+    float J[3][3] = {{0}};
     //computeBMatrix<qa,qb,qc>(X, J, B );
     //computeGradPhiBGradPhi<ORDER,qa,qb,qc>(B, func );
     computeBMatrix(qa,qb,qc,X,J,B );
@@ -499,7 +507,7 @@ public:
                                            float pnLocal[],
                                            float Y[]) const
   {
-      double X[8][3];
+      float X[8][3];
       int I=0;
       for( int k=0;k<ORDER+1;k+=ORDER )
       {
@@ -533,8 +541,8 @@ public:
       */
       for (int q=0;q<nPointsPerElement;q++)
       {
-          massMatrixLocal[q]=computeMassTerm(ORDER,q, X); 
-          computeStiffnessTerm(ORDER,q, X, [&] (const int i, const int j, const double val)
+          massMatrixLocal[q]=computeMassTerm<ORDER>(q, X); 
+          computeStiffnessTerm<ORDER>(q, X, [&] (const int i, const int j, const double val)
                   {
                    float localIncrement=val*pnLocal[j];
                    Y[i]+=localIncrement;
