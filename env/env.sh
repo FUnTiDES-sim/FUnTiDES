43b2d49b9ec3bf961e388733d885515d66b904af
Henri Calandra <henricalandra@netscape.net>
2025-02-06 13:27:33 +0100

    re-activate test dir in CMakeLists.txt

--- env/env.sh
+++ env/env.sh
@@ -1,11 +1,13 @@
 
-export _TPL_ROOT_DIR=~/src/proxyApp/buildInstall/installTPL
+export _TPL_ROOT_DIR=~/src/proxyApp/buildInstall/installTPL4
 export RAJA_DIR=${_TPL_ROOT_DIR}/raja/lib/cmake/raja
 export CAMP_DIR=${_TPL_ROOT_DIR}/raja/lib/cmake/camp
 export CHAI_DIR=${_TPL_ROOT_DIR}/chai/share/chai/cmake/
 export UMPIRE_DIR=${_TPL_ROOT_DIR}/chai/lib/cmake/umpire/
-kokkos_install_dir=~/src/proxyApp/buildInstall/installTPL/kokkos
+
+kokkos_install_dir=~/src/proxyApp/buildInstall/installTPL4/kokkos
 export KOKKOS_DIR=$kokkos_install_dir/lib/cmake/Kokkos
 export KOKKOS_INCLUDE_DIR=$kokkos_install_dir/include
+
 export SHIVA_DIR=/home/henri/src/proxyApp/buildInstall/installShiva
 
