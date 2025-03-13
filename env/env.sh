2591aa83a16609d1003c7c52baeebb035a870522
Henri Calandra <henricalandra@netscape.net>
2025-03-13 14:14:46 +0100

    adapt Raja to latest version

--- env/env.sh
+++ env/env.sh
@@ -1,11 +1,12 @@
 
-export _TPL_ROOT_DIR=~/src/proxyApp/buildInstall/installTPL4
+export _TPL_ROOT_DIR=~/src/proxyApp/buildInstall/installTPL
 export RAJA_DIR=${_TPL_ROOT_DIR}/raja/lib/cmake/raja
 export CAMP_DIR=${_TPL_ROOT_DIR}/raja/lib/cmake/camp
-export CHAI_DIR=${_TPL_ROOT_DIR}/chai/share/chai/cmake/
+#export CHAI_DIR=${_TPL_ROOT_DIR}/chai/share/chai/cmake/
+export CHAI_DIR=${_TPL_ROOT_DIR}/chai/lib/cmake/chai
 export UMPIRE_DIR=${_TPL_ROOT_DIR}/chai/lib/cmake/umpire/
 
-kokkos_install_dir=~/src/proxyApp/buildInstall/installTPL4/kokkos
+kokkos_install_dir=~/src/proxyApp/buildInstall/installTPL/kokkos
 export KOKKOS_DIR=$kokkos_install_dir/lib/cmake/Kokkos
 export KOKKOS_INCLUDE_DIR=$kokkos_install_dir/include
 
