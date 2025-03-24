0e83013dc8099a2af5f20045ee81ef2f3cac98a0
Alexis Bandet <alexis.bandet@inria.fr>
2025-03-24 10:51:44 +0100

    Add EZV (from EASYPAP) into compilation settings

--- env/env.sh
+++ env/env.sh
@@ -12,3 +12,6 @@ export KOKKOS_INCLUDE_DIR=$kokkos_install_dir/include
 export SHIVA_DIR=/home/henri/src/proxyApp/buildInstall/installShiva
 
 export CALIPER_DIR=/home/alexis/install/caliper
+
+export EASYPAP_DIR=/home/alexis/install/easypap
+export SCOTCH_DIR=/home/alexis/install/scotch
--- src/sem/main/argsparse.hpp
+++ src/sem/main/argsparse.hpp
@@ -1,6 +1,8 @@
 #ifndef ARGSPARSE_HPP
 #define ARGSPARSE_HPP
 
+#include <string>
+
 char *getCmdOption(char **begin, char **end, const std::string &option);
 bool cmdOptionExists(char **begin, char **end, const std::string &option);
 
