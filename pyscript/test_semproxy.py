b6048e0b4ea84444bddf5f7ff45a497446bb41b4
Alexis Bandet <alexis.bandet@inria.fr>
2025-04-25 16:54:30 +0200

    Adding pykokkos-base as submodule. Now Working

--- pyscript/test_semproxy.py
+++ pyscript/test_semproxy.py
@@ -1,8 +1,8 @@
 import pysem
 import libpykokkos as kokkos
 
-pysem.initialize_kokkos()
-# kokkos.initialize()
+# pysem.initialize_kokkos()
+kokkos.initialize()
 
 sem = pysem.SEMproxy(30,30,30,1000)
 sem.initFiniteElem()
@@ -11,5 +11,5 @@ sem.run()
 pysem.fence()
 sem = None
 pysem.fence()
-pysem.finalize_kokkos()
-# kokkos.finalize()
+# pysem.finalize_kokkos()
+kokkos.finalize()
