53d8c1b26ab3665b893f7cef648eb5a424922318
Jie Meng <jie.meng@total.com>
2025-05-04 08:54:48 -0500

    correct header path for compilation of SEMClassic

--- scripts/env_Maple_GH200.sh
+++ scripts/env_Maple_GH200.sh
@@ -2,6 +2,8 @@ module load cmake gcc/13.1.1 cuda/12.6.20
 
 export OMP_PROC_BIND=spread; export OMP_PLACES=threads
 
+export Kokkos_DIR=/shared/data1/Users/j0535952/work2025/ProgrammingModels/kokkos/kokkos/install_maple_rdc/lib64/cmake/Kokkos
+export CUDA_ROOT=/hrtc/apps/cuda/12.6.20/aarch64/rocky9/
 
 # by default: USE_SEMOPTIM is ON
 #
