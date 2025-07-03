0ed8d344e2fa5d4fa6da2c8ec9303e4bd46de1cf
Alexis Bandet <alexis.bandet@inria.fr>
2025-07-03 14:38:53 +0200

    Remove pybind11 internal compilation for pykokkos-base + various scripts

--- scripts/pyscript/test_semsolver.py
+++ scripts/pyscript/test_semsolver.py
@@ -9,47 +9,57 @@ import pysolver as Solver
 import pysem as Sem
 
 
-def print_global( arr ):
-  for idx, e in enumerate(arr):
-    if e != 0:
-      print(f"arr[{idx}] = {e}")
+def print_global(arr):
+    for idx, e in enumerate(arr):
+        if e != 0:
+            print(f"arr[{idx}] = {e}")
 
-def sourceTerm( time_n, f0 ):
-  o_tpeak = 1.0/f0;
-  pulse = 0.0;
-  if time_n <= -0.9*o_tpeak or time_n >= 2.9*o_tpeak:
+
+def sourceTerm(time_n, f0):
+    o_tpeak = 1.0 / f0
+    pulse = 0.0
+    if time_n <= -0.9 * o_tpeak or time_n >= 2.9 * o_tpeak:
+        return pulse
+
+    pi = 3.14157
+    lam = (f0 * pi) * (f0 * pi)
+    pulse = (
+        2.0
+        * lam
+        * (2.0 * lam * (time_n - o_tpeak) * (time_n - o_tpeak) - 1.0)
+        * np.exp(-lam * (time_n - o_tpeak) * (time_n - o_tpeak))
+    )
     return pulse
 
-  pi = 3.14157
-  lam = (f0*pi)*(f0*pi)
-  pulse = 2.0*lam*(2.0*lam*(time_n-o_tpeak)*(time_n-o_tpeak)-1.0)*np.exp( -lam*(time_n-o_tpeak)*(time_n-o_tpeak))
-  return pulse
 
-def initModel(model,nElements):
+def initModel(model, nElements):
     for i in range(nElements):
-        model[i]=1500.
+        model[i] = 1500.0
     return 0
 
-def initPressure(pressure,nDof):
+
+def initPressure(pressure, nDof):
     for i in range(nDof):
-        pressure[i,0]=0.
-        pressure[i,1]=0.
+        pressure[i, 0] = 0.0
+        pressure[i, 1] = 0.0
     return 0
 
-def getSnapshot(timeStep,i1,nx,ny,nz,hx,hy,hz,pnGlobal, normalize):
-  offset=nx*nz*(int(ny/2)-1)
-  grid=np.zeros((nx,nz))
-  for I in range(offset,offset+nx*nz):
-      i=(I-offset)%nx
-      j=int((I-offset-i)/nx);
-      grid[i,j]=pnGlobal[I,i1]
 
-  if normalize:
-      maxvalue = np.abs(grid).max()
-      if maxvalue != 0:
-        grid = grid / maxvalue
+def getSnapshot(timeStep, i1, nx, ny, nz, hx, hy, hz, pnGlobal, normalize):
+    offset = nx * nz * (int(ny / 2) - 1)
+    grid = np.zeros((nx, nz))
+    for I in range(offset, offset + nx * nz):
+        i = (I - offset) % nx
+        j = int((I - offset - i) / nx)
+        grid[i, j] = pnGlobal[I, i1]
 
-  return grid
+    if normalize:
+        maxvalue = np.abs(grid).max()
+        if maxvalue != 0:
+            grid = grid / maxvalue
+
+    return grid
+
 
 def get_element_number_from_point(ex, ey, ez, hx, hy, hz, x, y, z):
     eX = eY = eZ = 0
@@ -67,151 +77,175 @@ def get_element_number_from_point(ex, ey, ez, hx, hy, hz, x, y, z):
 
 
 def main():
-  kokkos.initialize()
-  # Add timing variables
-  start_time = time.time()
-  simulation_start = datetime.now()
-  iteration_times = []
+    kokkos.initialize()
+    # Add timing variables
+    start_time = time.time()
+    simulation_start = datetime.now()
+    iteration_times = []
 
-  print(f"Simulation started at: {simulation_start}")
+    print(f"Simulation started at: {simulation_start}")
 
-  order=2
-  domain_size=1500.
-  ex=100
-  ey=100
-  ez=100
-  hx=domain_size/ex
-  hy=domain_size/ey
-  hz=domain_size/ez
+    order = 2
+    domain_size = 1500.0
+    ex = 200
+    ey = 200
+    ez = 200
+    hx = domain_size / ex
+    hy = domain_size / ey
+    hz = domain_size / ez
 
-  nx=ex*order+1
-  ny=ey*order+1
-  nz=ez*order+1
+    nx = ex * order + 1
+    ny = ey * order + 1
+    nz = ez * order + 1
 
-  nDof=nx*ny*nz
+    nDof = nx * ny * nz
 
-  nElements=ex*ey*ez
-  nPointsPerElement=(order+1)*(order+1)*(order+1)
-  freqPrint=5
-  printOffset=int(nDof/freqPrint)
-  print("number of elements=",nElements)
+    nElements = ex * ey * ez
+    nPointsPerElement = (order + 1) * (order + 1) * (order + 1)
+    freqPrint = 5
+    printOffset = int(nDof / freqPrint)
+    print("number of elements=", nElements)
 
-  memspace = kokkos.CudaUVMSpace
-  layout = kokkos.LayoutLeft
+    memspace = kokkos.CudaUVMSpace
+    layout = kokkos.LayoutLeft
 
-  kk_model = kokkos.array([nElements], dtype=kokkos.float32, space=memspace, layout=layout)
-  model = np.array(kk_model, copy=False)
-  initModel(model,nElements)
+    kk_model = kokkos.array(
+        [nElements], dtype=kokkos.float32, space=memspace, layout=layout
+    )
+    model = np.array(kk_model, copy=False)
+    initModel(model, nElements)
 
-  mesh = Solver.SEMmesh(ex, ey, ez, domain_size, domain_size, domain_size, order, 30, False)
-  myInfo = Solver.SEMinfo()
-  myInfo.numberOfNodes = nx * ny * nz
-  myInfo.numberOfElements = nElements
-  myInfo.numberOfPointsPerElement = nPointsPerElement
-  myInfo.numberOfInteriorNodes = mesh.getNumberOfInteriorNodes()
+    mesh = Solver.SEMmesh(
+        ex, ey, ez, domain_size, domain_size, domain_size, order, 30, False
+    )
+    myInfo = Solver.SEMinfo()
+    myInfo.numberOfNodes = nx * ny * nz
+    myInfo.numberOfElements = nElements
+    myInfo.numberOfPointsPerElement = nPointsPerElement
+    myInfo.numberOfInteriorNodes = mesh.getNumberOfInteriorNodes()
 
-  solver = Solver.SEMsolver()
-  solver.computeFEInit(myInfo, mesh)
-  solver.set_model(kk_model)
+    solver = Solver.SEMsolver()
+    solver.computeFEInit(myInfo, mesh)
+    solver.set_model(kk_model)
 
-  nodesList=np.zeros((nElements,nPointsPerElement),dtype=int)
+    nodesList = np.zeros((nElements, nPointsPerElement), dtype=int)
 
-  # allocate pressure
-  print("Allocating Pressure...")
-  kk_pnGlobal = kokkos.array([nDof, 2], dtype=kokkos.float32, space=memspace, layout=layout)
-  pnGlobal = np.array(kk_pnGlobal, copy=False)
-  initPressure(pnGlobal, nDof)
+    # allocate pressure
+    print("Allocating Pressure...")
+    kk_pnGlobal = kokkos.array(
+        [nDof, 2], dtype=kokkos.float32, space=memspace, layout=layout
+    )
+    pnGlobal = np.array(kk_pnGlobal, copy=False)
+    initPressure(pnGlobal, nDof)
 
-  # source term
-  f0=5
-  # time step and sampling
-  timeStep=0.001
-  nTimeSteps=1500
-  numberOfRHS=1
-  xs=ex*hx/2
-  ys=ey*hy/2
-  zs=ez*hz/2
+    # source term
+    f0 = 5
+    # time step and sampling
+    timeStep = 0.001
+    nTimeSteps = 3000
+    numberOfRHS = 1
+    xs = ex * hx / 2
+    ys = ey * hy / 2
+    zs = ez * hz / 2
 
-  kk_RHSElement = kokkos.array([numberOfRHS], dtype=kokkos.int32, space=memspace, layout=layout)
-  RHSElement=np.array(kk_RHSElement, copy=False)
-  RHSElement[0] = get_element_number_from_point(ex,ey,ez,hx,hy,hz,xs,ys,zs)
-  print("RHS element number ", RHSElement[0])
+    kk_RHSElement = kokkos.array(
+        [numberOfRHS], dtype=kokkos.int32, space=memspace, layout=layout
+    )
+    RHSElement = np.array(kk_RHSElement, copy=False)
+    RHSElement[0] = get_element_number_from_point(ex, ey, ez, hx, hy, hz, xs, ys, zs)
+    print("RHS element number ", RHSElement[0])
 
-  # compute source term
-  kk_RHSTerm = kokkos.array([numberOfRHS, nTimeSteps], dtype=kokkos.float32, space=memspace, layout=layout)
-  RHSTerm = np.array(kk_RHSTerm, copy=False)
-  for i in range(nTimeSteps):
-      RHSTerm[0,i]=sourceTerm(i*timeStep,f0)
+    # compute source term
+    kk_RHSTerm = kokkos.array(
+        [numberOfRHS, nTimeSteps], dtype=kokkos.float32, space=memspace, layout=layout
+    )
+    RHSTerm = np.array(kk_RHSTerm, copy=False)
+    for i in range(nTimeSteps):
+        RHSTerm[0, i] = sourceTerm(i * timeStep, f0)
 
-  # setup graphic display
-  grid=np.zeros((nx,nz))
-  fig, ax = plt.subplots()
-  cmpvalue = 20
-  im = ax.imshow(grid, cmap='viridis', interpolation='nearest', vmin=-cmpvalue, vmax=cmpvalue)
+    # setup graphic display
+    grid = np.zeros((nx, nz))
+    fig, ax = plt.subplots()
+    cmpvalue = 20
+    im = ax.imshow(
+        grid, cmap="viridis", interpolation="nearest", vmin=-cmpvalue, vmax=cmpvalue
+    )
 
-  plt.colorbar(im, ax=ax, label="Intensity")
-  plt.title("2D Slice of a Float32 Array")
-  plt.xlabel("X-axis")
-  plt.ylabel("Z-axis")
-  # propagate one step forward in time
-  i1=0
-  i2=1
-  maxval=1
+    plt.colorbar(im, ax=ax, label="Intensity")
+    plt.title("2D Slice of a Float32 Array")
+    plt.xlabel("X-axis")
+    plt.ylabel("Z-axis")
+    # propagate one step forward in time
+    i1 = 0
+    i2 = 1
+    maxval = 1
 
-  for timeSample in range(nTimeSteps):
-     iter_start = time.time()
-     solver.computeOneStep(timeSample, order, nPointsPerElement, i1, i2, myInfo, kk_RHSTerm, kk_pnGlobal, kk_RHSElement)
+    for timeSample in range(nTimeSteps):
+        iter_start = time.time()
+        solver.computeOneStep(
+            timeSample,
+            order,
+            nPointsPerElement,
+            i1,
+            i2,
+            myInfo,
+            kk_RHSTerm,
+            kk_pnGlobal,
+            kk_RHSElement,
+        )
 
-     iter_time = time.time() - iter_start
-     iteration_times.append(iter_time)
+        iter_time = time.time() - iter_start
+        iteration_times.append(iter_time)
 
-     if timeSample%10==0:
-       print("sum pnGlobal[:, i1]", np.sum(pnGlobal[:, i1]))
-       print(f"Average iteration time: {np.mean(iteration_times):.4f} seconds")
-       elementSource=nodesList[RHSElement[0],0]
-       elapsed_time = time.time() - start_time
-       print("TIME")
-       print(f"Time iteration {timeSample}/{nTimeSteps}")
-       print(f"Elapsed time: {elapsed_time:.2f} seconds")
-       print(f"Average iteration time: {np.mean(iteration_times):.4f} seconds")
-       print(f"Min pressure: {np.min(pnGlobal)}")
-       print(f"Max pressure: {np.max(pnGlobal)}")
-       print()
+        if timeSample % 1000 == 0:
+            print("sum pnGlobal[:, i1]", np.sum(pnGlobal[:, i1]))
+            print(f"Average iteration time: {np.mean(iteration_times):.4f} seconds")
+            elementSource = nodesList[RHSElement[0], 0]
+            elapsed_time = time.time() - start_time
+            print()
 
-       # plotting
-       grid=getSnapshot(timeStep,i1,nx,ny,nz,hx,hy,hz,pnGlobal,False)
-       im.set_array(grid)  # Update plot with new values
-       plt.draw()  # Redraw the figure with updated data
-       plt.ioff
-       plt.savefig(f"snap0{timeSample:0{5}d}.png")
+        if timeSample % 100 == 0:
+            print(f"Time {timeSample} / {nTimeSteps}")
 
-     tmp=i1
-     i1=i2
-     i2=tmp
+        if timeSample % 10 == 0:
+            # plotting
+            grid = getSnapshot(timeStep, i1, nx, ny, nz, hx, hy, hz, pnGlobal, False)
+            im.set_array(grid)  # Update plot with new values
+            plt.draw()  # Redraw the figure with updated data
+            plt.ioff
+            plt.savefig(f"snap0{timeSample:0{5}d}.png")
 
-  # Print final timing statistics
-  end_time = time.time()
-  simulation_end = datetime.now()
-  total_time = end_time - start_time
+        # Swap pn and pn+1
+        tmp = i1
+        i1 = i2
+        i2 = tmp
 
-  print("\nSimulation Statistics:")
-  print(f"Start time: {simulation_start}")
-  print(f"End time: {simulation_end}")
-  print(f"Total runtime: {total_time:.2f} seconds")
-  print(f"Average iteration time: {np.mean(iteration_times):.4f} seconds")
-  print(f"Min iteration time: {np.min(iteration_times):.4f} seconds")
-  print(f"Max iteration time: {np.max(iteration_times):.4f} seconds")
+    # Print final timing statistics
+    end_time = time.time()
+    simulation_end = datetime.now()
+    total_time = end_time - start_time
 
-  # release kokkos arrays and vectors
-  # del nodesList, nodesCoordsX, nodesCoordsY,nodesCoordsZ,model,pnGlobal
-  # del massMatrixGlobal,yGlobal,RHSElement,RHSTerm
-  del solver
-  del mesh
-  del myInfo
+    print("\nSimulation Statistics:")
+    print(f"Start time: {simulation_start}")
+    print(f"End time: {simulation_end}")
+    print(f"Total runtime: {total_time:.2f} seconds")
+    print(f"Average iteration time: {np.mean(iteration_times):.4f} seconds")
+    print(f"Min iteration time: {np.min(iteration_times):.4f} seconds")
+    print(f"Max iteration time: {np.max(iteration_times):.4f} seconds")
 
-  kokkos.finalize()
-  print( "end of  computation")
-  print("we close kokkos")
+    # release kokkos arrays and vectors
+    del kk_pnGlobal
+    del kk_model
+    del kk_RHSTerm
+    del kk_RHSElement
+    del solver
+    del mesh
+    del myInfo
+
+    kokkos.finalize()
+    print("end of  computation")
+    print("we close kokkos")
+
 
 if __name__ == "__main__":
-  main()
+    main()
