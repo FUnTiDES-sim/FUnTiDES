2b6ef64729461b95482827d06b90ff92e4bc8619
Alexis Bandet <alexis.bandet@inria.fr>
2025-08-25 14:06:29 +0200

    Forget files

---
+++ src/model/model_api/src/model_struct.cc
@@ -0,0 +1,8 @@
+#include "model_struct.h"
+
+namespace model {
+    template class ModelStruct<float, int>;
+    template class ModelStruct<float, long>;
+    template class ModelStruct<double, int>;
+    template class ModelStruct<double, long>;
+}
