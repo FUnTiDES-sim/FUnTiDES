eb1d572fcabd060246a03ac28edf4e8c30dec0fc
Alexis Bandet <alexis.bandet@inria.fr>
2025-09-17 09:58:30 +0200

    Format the code according to google standard (#66)
    * Update clang-format
    
    * Solver: reformatting with Google standard
    
    * Adding CONTRIBUTING.md

--- src/model/impl/model/struct/src/model_struct.cc
+++ src/model/impl/model/struct/src/model_struct.cc
@@ -1,8 +1,9 @@
 #include "model_struct.h"
 
-namespace model {
-    template class ModelStruct<float, int, 1>;
-    template class ModelStruct<float, int, 2>;
-    template class ModelStruct<float, int, 3>;
-    template class ModelStruct<float, int, 4>;
-}
+namespace model
+{
+template class ModelStruct<float, int, 1>;
+template class ModelStruct<float, int, 2>;
+template class ModelStruct<float, int, 3>;
+template class ModelStruct<float, int, 4>;
+}  // namespace model
