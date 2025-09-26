27915cae9c00346aab9fd9376cca3684b5701519
acitrain <60715545+acitrain@users.noreply.github.com>
2025-09-26 16:37:53 +0200

    Separate mass and stiffness computation (#97)

--- src/solver/impl/include/sem_solver_impl.h
+++ src/solver/impl/include/sem_solver_impl.h
@@ -142,12 +142,27 @@ void SEMsolver<ORDER, INTEGRAL_TYPE, MESH_TYPE>::computeElementContributions(
     }
   }
 
-  INTEGRAL_TYPE::computeMassMatrixAndStiffnessVector(
-      elementNumber, m_mesh.getNumberOfPointsPerElement(), cornerCoords,
-      m_precomputedIntegralData, massMatrixLocal, pnLocal, Y);
-
   auto const inv_model2 = 1.0f / (m_mesh.getModelVpOnElement(elementNumber) *
                                   m_mesh.getModelVpOnElement(elementNumber));
+
+  // Stiffness term
+
+  for (int i = 0; i < m_mesh.getNumberOfPointsPerElement(); ++i)
+  {
+    Y[i] = 0;
+    massMatrixLocal[i] = 0;
+  }
+
+  INTEGRAL_TYPE::computeMassTerm(
+      cornerCoords,
+      [&](const int j, const real_t val) { massMatrixLocal[j] += val; });
+
+  INTEGRAL_TYPE::computeStiffnessTerm(
+      cornerCoords, [&](const int i, const int j, const real_t val) {
+        float localIncrement = val * pnLocal[j];
+        Y[i] += localIncrement;
+      });
+
   for (int i = 0; i < m_mesh.getNumberOfPointsPerElement(); ++i)
   {
     int x = i % dim;
