d5063a4738efacb72842c65cf2cf988b6269c6db
Randolph Settgast <rrsettgast@gmail.com>
2025-09-25 21:30:43 -0700

    clang-format 13

--- src/model/api/include/builder.h
+++ src/model/api/include/builder.h
@@ -9,12 +9,13 @@ namespace model
 template <typename FloatType, typename ScalarType>
 class ModelBuilderBase
 {
-public:
+ public:
   ModelBuilderBase() = default;
   ~ModelBuilderBase() = default;
 
   static constexpr int MAX_ORDER = 5;
 
-  virtual std::shared_ptr<model::ModelApi<FloatType, ScalarType>> getModel() const = 0;
+  virtual std::shared_ptr<model::ModelApi<FloatType, ScalarType>> getModel()
+      const = 0;
 };
 }  // namespace model
