e4cff5975396359cc9249ec6b918f73be04bf1e1
acitrain <60715545+acitrain@users.noreply.github.com>
2025-10-02 17:10:39 +0200

    Add density inside the acoustic wave solver (#122)

--- src/solver/impl/include/sem_solver_impl.h
+++ src/solver/impl/include/sem_solver_impl.h
@@ -145,18 +145,13 @@ void SEMsolver<ORDER, INTEGRAL_TYPE, MESH_TYPE>::computeElementContributions(
   }
 
   real_t inv_model2 = 0.0f;
+  real_t inv_density = 0.0f;
   if (!isModelOnNodes)
   {
     inv_model2 = 1.0f / (m_mesh.getModelVpOnElement(elementNumber) *
-                         m_mesh.getModelVpOnElement(elementNumber));
-  }
-
-  // Stiffness term
-
-  for (int i = 0; i < m_mesh.getNumberOfPointsPerElement(); ++i)
-  {
-    Y[i] = 0;
-    massMatrixLocal[i] = 0;
+                         m_mesh.getModelVpOnElement(elementNumber) *
+                         m_mesh.getModelRhoOnElement(elementNumber));
+    inv_density = 1.0f / m_mesh.getModelRhoOnElement(elementNumber);
   }
 
   INTEGRAL_TYPE::computeMassTerm(
@@ -178,9 +173,12 @@ void SEMsolver<ORDER, INTEGRAL_TYPE, MESH_TYPE>::computeElementContributions(
     if (isModelOnNodes)
     {
       inv_model2 = 1.0f / (m_mesh.getModelVpOnNodes(gIndex) *
-                           m_mesh.getModelVpOnNodes(gIndex));
+                           m_mesh.getModelVpOnNodes(gIndex) *
+                           m_mesh.getModelRhoOnNodes(gIndex));
+      inv_density = 1.0f / m_mesh.getModelRhoOnNodes(gIndex);
     }
     massMatrixLocal[i] *= inv_model2;
+    Y[i] *= inv_density;
     ATOMICADD(massMatrixGlobal[gIndex], massMatrixLocal[i]);
     ATOMICADD(yGlobal[gIndex], Y[i]);
   }
