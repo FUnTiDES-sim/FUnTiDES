e217d1348a102c7f65a8df130f94d7a70d11949f
Alexis Bandet <alexis.bandet@inria.fr>
2025-10-07 17:23:50 +0200

    Feature/fd model (#138)
    * moving grid into model folder for fd
    
    * Grid cleaner code
    
    * google format
    
    * better doc in fdtd grids
    
    * split grid in multiple entities

--- src/main/fd/src/fdtd_solver.cpp
+++ src/main/fd/src/fdtd_solver.cpp
@@ -19,16 +19,16 @@ void fdtd_solver::compute_one_step(int itime,int i1,int i2)
 {
 
   int x3=m_stencils.lx;
-  int x4=m_grids.nx - m_stencils.lx;
+  int x4=m_grids.nx() - m_stencils.lx;
   int y3=m_stencils.ly;
-  int y4=m_grids.ny - m_stencils.ly;
+  int y4=m_grids.ny() - m_stencils.ly;
   int z3=m_stencils.lz;
-  int z4=m_grids.nz - m_stencils.lz;
+  int z4=m_grids.nz() - m_stencils.lz;
   m_kernels.addRHS( itime, i2,
-                   m_grids.nx, m_grids.ny, m_grids.nz,
+                   m_grids.nx(), m_grids.ny(), m_grids.nz(),
                    m_stencils.lx, m_stencils.ly, m_stencils.lz,
                    m_source_receivers.xsrc, m_source_receivers.ysrc, m_source_receivers.zsrc,
-                   m_grids.vp,
+                   m_grids.vp(),
                    m_kernels.RHSTerm,
                    m_kernels.pnGlobal );
                    
@@ -37,20 +37,20 @@ void fdtd_solver::compute_one_step(int itime,int i1,int i2)
   // inner points
  
   m_kernels.inner3D( i1, i2,
-                    m_grids.nx, m_grids.ny, m_grids.nz,
+                    m_grids.nx(), m_grids.ny(), m_grids.nz(),
                     m_stencils.lx, m_stencils.ly, m_stencils.lz,
                     x3, x4, y3, y4, z3, z4,
                     m_stencils.coef0,
                     m_stencils.coefx,
                     m_stencils.coefy,
                     m_stencils.coefz,
-                    m_grids.vp,
+                    m_grids.vp(),
                     m_kernels.pnGlobal );
   //printf("inner3D done\n");
   FDFENCE
   // apply sponge boundary to wavefield
   m_kernels.applySponge( i1, i2,
-                        m_grids.nx, m_grids.ny, m_grids.nz,
+                        m_grids.nx(), m_grids.ny(), m_grids.nz(),
                         m_stencils.lx, m_stencils.ly, m_stencils.lz,
                         x3, x4, y3, y4, z3, z4,
                         m_kernels.spongeArray,
