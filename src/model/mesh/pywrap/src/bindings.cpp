9a6e039bbdf809c05f66aebbe21954f6b58bc9cf
Alexis Bandet <alexis.bandet@inria.fr>
2025-10-09 14:51:17 +0200

    fix pywrap

--- src/model/mesh/pywrap/src/bindings.cpp
+++ src/model/mesh/pywrap/src/bindings.cpp
@@ -16,70 +16,70 @@ PYBIND11_MODULE(model, m)
   m.attr("__name__") = "pyproxys.model";
 
   // Bind ModelApi
-  model::mesh::bind_modelapi<float, int>(m);
-  model::mesh::bind_modelapi<double, int>(m);
-  model::mesh::bind_modelapi<float, long>(m);
-  model::mesh::bind_modelapi<double, long>(m);
+  model::bind_modelapi<float, int>(m);
+  model::bind_modelapi<double, int>(m);
+  model::bind_modelapi<float, long>(m);
+  model::bind_modelapi<double, long>(m);
 
   // Bind ModelStruct
   for (int order = 1; order <= 3; ++order)
   {
-    model::mesh::orderDispatch(order, [&](auto order_tag) {
+    model::orderDispatch(order, [&](auto order_tag) {
       constexpr int ord = decltype(order_tag)::value;
-      model::mesh::bind_modelstruct<float, int, ord>(m);
-      model::mesh::bind_modelstruct<double, int, ord>(m);
-      model::mesh::bind_modelstruct<float, long, ord>(m);
-      model::mesh::bind_modelstruct<double, long, ord>(m);
+      model::bind_modelstruct<float, int, ord>(m);
+      model::bind_modelstruct<double, int, ord>(m);
+      model::bind_modelstruct<float, long, ord>(m);
+      model::bind_modelstruct<double, long, ord>(m);
       return nullptr;
     });
   }
 
   // Bind ModelStructData
-  model::mesh::bind_modelstructdata<float, int>(m);
-  model::mesh::bind_modelstructdata<double, int>(m);
-  model::mesh::bind_modelstructdata<float, long>(m);
-  model::mesh::bind_modelstructdata<double, long>(m);
+  model::bind_modelstructdata<float, int>(m);
+  model::bind_modelstructdata<double, int>(m);
+  model::bind_modelstructdata<float, long>(m);
+  model::bind_modelstructdata<double, long>(m);
 
   // Bind ModelUnstruct
-  model::mesh::bind_modelunstruct<float, int>(m);
-  model::mesh::bind_modelunstruct<double, int>(m);
-  model::mesh::bind_modelunstruct<float, long>(m);
-  model::mesh::bind_modelunstruct<double, long>(m);
+  model::bind_modelunstruct<float, int>(m);
+  model::bind_modelunstruct<double, int>(m);
+  model::bind_modelunstruct<float, long>(m);
+  model::bind_modelunstruct<double, long>(m);
 
   // Bind ModelUnstructData
-  model::mesh::bind_modelunstructdata<float, int>(m);
-  model::mesh::bind_modelunstructdata<double, int>(m);
-  model::mesh::bind_modelunstructdata<float, long>(m);
-  model::mesh::bind_modelunstructdata<double, long>(m);
+  model::bind_modelunstructdata<float, int>(m);
+  model::bind_modelunstructdata<double, int>(m);
+  model::bind_modelunstructdata<float, long>(m);
+  model::bind_modelunstructdata<double, long>(m);
 
   // Bind ModelBuilderBase
-  model::mesh::bind_modelbuilderbase<float, int>(m);
-  model::mesh::bind_modelbuilderbase<double, int>(m);
-  model::mesh::bind_modelbuilderbase<float, long>(m);
-  model::mesh::bind_modelbuilderbase<double, long>(m);
+  model::bind_modelbuilderbase<float, int>(m);
+  model::bind_modelbuilderbase<double, int>(m);
+  model::bind_modelbuilderbase<float, long>(m);
+  model::bind_modelbuilderbase<double, long>(m);
 
   // Bind CartesianStructBuilder
   for (int order = 1; order <= 3; ++order)
   {
-    model::mesh::orderDispatch(order, [&](auto order_tag) {
+    model::orderDispatch(order, [&](auto order_tag) {
       constexpr int ord = decltype(order_tag)::value;
-      model::mesh::bind_cartesian_struct_builder<float, int, ord>(m);
-      model::mesh::bind_cartesian_struct_builder<double, int, ord>(m);
-      model::mesh::bind_cartesian_struct_builder<float, long, ord>(m);
-      model::mesh::bind_cartesian_struct_builder<double, long, ord>(m);
+      model::bind_cartesian_struct_builder<float, int, ord>(m);
+      model::bind_cartesian_struct_builder<double, int, ord>(m);
+      model::bind_cartesian_struct_builder<float, long, ord>(m);
+      model::bind_cartesian_struct_builder<double, long, ord>(m);
       return nullptr;
     });
   }
 
   // Bind CartesianParams
-  model::mesh::bind_cartesian_unstruct_params<float, int>(m);
-  model::mesh::bind_cartesian_unstruct_params<double, int>(m);
-  model::mesh::bind_cartesian_unstruct_params<float, long>(m);
-  model::mesh::bind_cartesian_unstruct_params<double, long>(m);
+  model::bind_cartesian_unstruct_params<float, int>(m);
+  model::bind_cartesian_unstruct_params<double, int>(m);
+  model::bind_cartesian_unstruct_params<float, long>(m);
+  model::bind_cartesian_unstruct_params<double, long>(m);
 
   // Bind CartesianUnstructBuilder
-  model::mesh::bind_cartesian_unstruct_builder<float, int>(m);
-  model::mesh::bind_cartesian_unstruct_builder<double, int>(m);
-  model::mesh::bind_cartesian_unstruct_builder<float, long>(m);
-  model::mesh::bind_cartesian_unstruct_builder<double, long>(m);
+  model::bind_cartesian_unstruct_builder<float, int>(m);
+  model::bind_cartesian_unstruct_builder<double, int>(m);
+  model::bind_cartesian_unstruct_builder<float, long>(m);
+  model::bind_cartesian_unstruct_builder<double, long>(m);
 }
