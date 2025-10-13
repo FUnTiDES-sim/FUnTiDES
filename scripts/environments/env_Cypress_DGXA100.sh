6e5cbb02d13f352f35b4252491e8eaf3ae0238ff
Gaetan <gaetan.fuss.fr@gmail.com>
2025-10-13 17:39:13 +0200

    fix typo

--- scripts/environments/env_Cypress_DGXA100.sh
+++ scripts/environments/env_Cypress_DGXA100.sh
@@ -1,4 +1,12 @@
 module av
+source "/apps/modules/modulefiles3/supporting_scripts/modver_config.sh"
+
+#Don't use spider cache. This hopefully makes modules v2 and v3 work nicely together
+export LMOD_IGNORE_CACHE=1
+
+# activate modules 3
+source $SUPPORTSCRIPT_3_DIR/activateMod3
+module av
 module load cmake gcc/11.4.1 cuda/12.6.20
 module list
 
--- scripts/environments/env_Maple_GH200.sh
+++ scripts/environments/env_Maple_GH200.sh
@@ -1,12 +1,3 @@
-source "/apps/modules/modulefiles3/supporting_scripts/modver_config.sh"
-
-#Don't use spider cache. This hopefully makes modules v2 and v3 work nicely together
-export LMOD_IGNORE_CACHE=1
-
-# activate moddules 3
-source $SUPPORTSCRIPT_3_DIR/activateMod3
-module av
-
 module load cmake gcc/13.1.1 cuda/12.6.20
 module list
 
