23752c4932b76bcaf2369b042b10a10459b6c5ad
aurelien <aurelien.citrain@inria.fr>
2025-10-14 14:06:57 +0200

    Modify test after introduction if modelLocation in solver

--- tests/units/solver/pywrap/test_solver_unstruct.py
+++ tests/units/solver/pywrap/test_solver_unstruct.py
@@ -49,32 +49,33 @@ class TestSolverUnstruct:
         "implem", [Solver.ImplemType.MAKUTU, Solver.ImplemType.SHIVA]
     )
     def test_solver_one_step(self, unstruct, implem):
-        sd, _, builder = unstruct
+        sd, _, builder, is_model_on_nodes = unstruct  # Déstructure avec le booléen
         n_rhs = 2
         dt = 0.001
         time_sample = 1
         n_time_steps = 1
         f0 = 5.0
-
         model = builder.get_model()
-
+        
         # TODO remove when we reactivate SHIVA
         if implem == Solver.ImplemType.SHIVA:
             return
-
+        
+        # Convertir le booléen en ModelLocationType
+        model_location = (Solver.ModelLocationType.ON_NODES if is_model_on_nodes 
+                         else Solver.ModelLocationType.ON_ELEMENTS)
+        
         solver = Solver.create_solver(
-            Solver.MethodType.SEM, implem, Solver.MeshType.UNSTRUCT, sd.order
+            Solver.MethodType.SEM, implem, Solver.MeshType.UNSTRUCT, 
+            model_location, sd.order
         )
-
+        
         solver.compute_fe_init(model)
-
         kk_pnGlobal, _ = Utils.allocate_pressure(sd.n_dof)
         kk_RHSElement, _ = Utils.allocate_rhs_element(n_rhs, sd.ex, sd.ey, sd.ez)
         kk_RHSWeights, _ = Utils.allocate_rhs_weight(n_rhs, model)
         kk_RHSTerm, _ = Utils.allocate_rhs_term(n_rhs, n_time_steps, dt, f0)
-
         data = Solver.SEMsolverData(
             0, 1, kk_RHSTerm, kk_pnGlobal, kk_RHSElement, kk_RHSWeights
         )
-
         solver.compute_one_step(dt, time_sample, data)
