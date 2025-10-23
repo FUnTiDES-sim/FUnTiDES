46ac2806cdab465cb066ec4adb4c450ba8d4e436
Henri Calandra <henricalandra@netscape.net>
2025-10-23 15:22:11 +0200

    add pml abc

---
+++ src/solver/fd/include/fd_solver.h
@@ -0,0 +1,57 @@
+//************************************************************************
+//   proxy application v.0.0.1
+//
+//  semproxy.hpp: the main interface of SEM proxy application
+//
+//************************************************************************
+
+#ifndef FDTD_SOLVER_HPP_
+#define FDTD_SOLVER_HPP_
+
+#include <utils.h>
+#include <memory>
+
+#include <fd_grids.h>
+#include "fd_kernels.h"
+#include "fd_abckernels.h"
+#include "fd_source_receivers.h"
+#include "fd_stencils.h"
+
+/**
+ * @class fdtd_solver
+ */
+
+class FdtdSolver
+{
+ public:
+  /**
+   * @brief Constructor of the SEMproxy class
+   */
+
+  FdtdSolver(model::fdgrid::FdtdGrids& grids,
+             fdtd::kernel::FdtdKernels& kernels,
+             fdtd::abckernel::FdtdAbcKernels& abckernels,
+             fdtd::stencils::FdtdStencils& stencils,
+             FdtdSourceReceivers& source_receivers): m_grids(grids),
+                                                     m_kernels(kernels),
+                                                     m_abckernels(abckernels),
+                                                     m_stencils(stencils),
+                                                     m_source_receivers(source_receivers){};
+
+  /**
+   * @brief Destructor of the SEMproxy class
+   */
+  ~FdtdSolver(){};
+
+  // compute one step
+  void compute_one_step(int itime, int i1, int i2);
+
+ private:
+  model::fdgrid::FdtdGrids& m_grids;
+  fdtd::kernel::FdtdKernels& m_kernels;
+  fdtd::abckernel::FdtdAbcKernels& m_abckernels;
+  fdtd::stencils::FdtdStencils& m_stencils;
+  FdtdSourceReceivers& m_source_receivers;
+};
+
+#endif /* FDTD_SOLVER_HPP_ */
