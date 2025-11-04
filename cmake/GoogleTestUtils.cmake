e853ffe64d73240b3fe2a02c84681fd60625f324
Stefano Frambati <stefano.frambati@totalenergies.com>
2025-11-04 11:07:30 +0100

    reverted change on gtest (another PR takes care of it)

--- cmake/GoogleTestUtils.cmake
+++ cmake/GoogleTestUtils.cmake
@@ -17,8 +17,5 @@ function(add_gtest TEST_NAME TEST_FILE EXTRA_LINKS)
 
     target_link_kokkos_if_enabled(${TEST_NAME})
 
-    message(STATUS "Adding gtest: ${TEST_NAME} with file ${TEST_FILE} and links ${EXTRA_LINKS}")
-    gtest_discover_tests(${TEST_NAME}
-        DISCOVERY_TIMEOUT 60
-    )
+gtest_discover_tests(${TEST_NAME})
 endfunction()
