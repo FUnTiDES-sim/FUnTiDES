6b935973e6dffad2814b8ae751a8d8636d65b7d4
Gaetan <159525405+Bubusch@users.noreply.github.com>
2025-11-04 14:49:00 +0100

    cmake auto generate solver sources (#171)

--- src/solver/impl/src/makutu/SEMQ3_unstruct_makutu_solver_modelonelements.cpp
+++
@@ -1,8 +0,0 @@
-#include <model_unstruct.h>
-
-#include "sem_solver_impl.h"
-
-constexpr int ORDER = 3;
-template class SEMsolver<
-    ORDER, IntegralTypeSelector<ORDER, IntegralType::MAKUTU>::type,
-    model::ModelUnstruct<float, int>, false>;
