2001b41abc8792449015c323c4f36f1810a9299c
Stefano Frambati <stefano.frambati@totalenergies.com>
2025-11-04 11:07:52 +0100

    whitespaces

--- cmake/GoogleTestUtils.cmake
+++ cmake/GoogleTestUtils.cmake
@@ -17,5 +17,5 @@ function(add_gtest TEST_NAME TEST_FILE EXTRA_LINKS)
 
     target_link_kokkos_if_enabled(${TEST_NAME})
 
-gtest_discover_tests(${TEST_NAME})
+    gtest_discover_tests(${TEST_NAME})
 endfunction()
