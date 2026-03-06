237ee867006ba2fafe47ed45f45a274dfb5db546
Alexis BANDET <alexis.bandet@inria.fr>
2026-03-06 16:45:09 +0100

    [fix] install with google test

--- cmake/GoogleTestConfig.cmake
+++ cmake/GoogleTestConfig.cmake
@@ -11,9 +11,13 @@ endif()
 FetchContent_Declare(
   googletest
   URL https://github.com/google/googletest/archive/03597a01ee50ed33e9dfd640b249b4be3799d395.zip
+  EXCLUDE_FROM_ALL
 )
+
 # For Windows: Prevent overriding the parent project's compiler/linker settings
 set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+set(INSTALL_GTEST OFF CACHE BOOL "" FORCE)
+set(INSTALL_GMOCK OFF CACHE BOOL "" FORCE)
 FetchContent_MakeAvailable(googletest)
 include(GoogleTest)
 enable_testing()
