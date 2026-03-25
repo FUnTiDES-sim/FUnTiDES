fdae1ab71fca18390fc2f636292f4123f3e62af9
Alexis BANDET <alexis.bandet@inria.fr>
2026-03-25 15:55:06 +0100

    address comments

--- cmake/KokkosConfig.cmake
+++
@@ -1,7 +0,0 @@
-#-------------------------------------------------------------------
-# Kokkos configuration
-#-------------------------------------------------------------------
-
-message(STATUS "Building the application with Kokkos.")
-
-find_package(Kokkos REQUIRED)
