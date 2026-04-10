56644542c969697c32b8018a5a110a235df8915d
aurelien <aurelien.citrain@inria.fr>
2026-04-10 14:14:58 +0200

    Remove deep copy in elastoacosutic logic

--- src/solver/fe/pywrap/include/bindings_rhs.h
+++ src/solver/fe/pywrap/include/bindings_rhs.h
@@ -55,55 +55,26 @@ void bind_rhs_elastic(py::module_& m)
 
 void bind_rhs_acoustoelastic(py::module_& m)
 {
-  using real_type = typename ARRAY_REAL_VIEW::value_type;
-  using int_type = typename VECTOR_INT_VIEW::value_type;
-
-  // Bind RhsAcoustoElastic (inherits from Rhs)
   py::class_<RhsAcoustoElastic, Rhs, std::shared_ptr<RhsAcoustoElastic>>(
       m, "RhsAcoustoElastic")
-      .def(py::init([](Kokkos::Experimental::python_view_type_t<ARRAY_REAL_VIEW>
-                           acoustic_term_py,
-                       Kokkos::Experimental::python_view_type_t<VECTOR_INT_VIEW>
-                           element_py,
-                       Kokkos::Experimental::python_view_type_t<ARRAY_REAL_VIEW>
-                           weights_py,
-                       Kokkos::Experimental::python_view_type_t<ARRAY_REAL_VIEW>
-                           elastic_termx_py,
-                       Kokkos::Experimental::python_view_type_t<ARRAY_REAL_VIEW>
-                           elastic_termy_py,
-                       Kokkos::Experimental::python_view_type_t<ARRAY_REAL_VIEW>
-                           elastic_termz_py) {
-             // Lambda simplifiée : On reçoit déjà une View (Host), on en crée
-             // une sur le Device
-             auto copy_to_device_2d = [](auto& host_view,
-                                         const std::string& name) {
-               ARRAY_REAL_VIEW d_view(name, host_view.extent(0),
-                                      host_view.extent(1));
-               Kokkos::deep_copy(d_view, host_view);
-               return d_view;
-             };
-
-             auto copy_to_device_1d = [](auto& host_view,
-                                         const std::string& name) {
-               VECTOR_INT_VIEW d_view(name, host_view.extent(0));
-               Kokkos::deep_copy(d_view, host_view);
-               return d_view;
-             };
-
-             return new RhsAcoustoElastic(
-                 copy_to_device_2d(acoustic_term_py, "rhs_acoustic_term"),
-                 copy_to_device_1d(element_py, "rhs_element"),
-                 copy_to_device_2d(weights_py, "rhs_weights"),
-                 copy_to_device_2d(elastic_termx_py, "rhs_elastic_term_x"),
-                 copy_to_device_2d(elastic_termy_py, "rhs_elastic_term_y"),
-                 copy_to_device_2d(elastic_termz_py, "rhs_elastic_term_z"));
-           }),
+      .def(py::init<Kokkos::Experimental::python_view_type_t<
+                        ARRAY_REAL_VIEW>,  // acoustic_term
+                    Kokkos::Experimental::python_view_type_t<
+                        VECTOR_INT_VIEW>,  // element
+                    Kokkos::Experimental::python_view_type_t<
+                        ARRAY_REAL_VIEW>,  // weights
+                    Kokkos::Experimental::python_view_type_t<
+                        ARRAY_REAL_VIEW>,  // elastic_termx
+                    Kokkos::Experimental::python_view_type_t<
+                        ARRAY_REAL_VIEW>,  // elastic_termy
+                    Kokkos::Experimental::python_view_type_t<
+                        ARRAY_REAL_VIEW>  // elastic_termz
+                    >(),
            py::arg("acoustic_term"), py::arg("element"), py::arg("weights"),
            py::arg("elastic_termx"), py::arg("elastic_termy"),
            py::arg("elastic_termz"))
       .def("print", &RhsAcoustoElastic::print);
 }
-
 }  // namespace fe
 }  // namespace solver
 #endif  // FUNTIDES_SOLVER_FE_PYWRAP_INCLUDE_BINDINGS_RHS_H_
